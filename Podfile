716178ee2d840bde12d2dd6c3d04cbcc522f985c
Zane Shannon <zcs@smileslaughs.com>
2014-10-09 15:50:34 -0700

    refactored to use cocoapods for external libraries. fixes #3

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+pod 'libHN'
+pod 'M13ProgressSuite'
+pod 'Shimmer'
+pod 'TUSafariActivity', '1.0.0'
+pod 'TWTSideMenuViewController'
+pod 'PocketAPI'
+pod 'MCSwipeTableViewCell'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// M13ProgressSuite
+#define COCOAPODS_POD_AVAILABLE_M13ProgressSuite
+#define COCOAPODS_VERSION_MAJOR_M13ProgressSuite 1
+#define COCOAPODS_VERSION_MINOR_M13ProgressSuite 2
+#define COCOAPODS_VERSION_PATCH_M13ProgressSuite 5
+
+// MCSwipeTableViewCell
+#define COCOAPODS_POD_AVAILABLE_MCSwipeTableViewCell
+#define COCOAPODS_VERSION_MAJOR_MCSwipeTableViewCell 2
+#define COCOAPODS_VERSION_MINOR_MCSwipeTableViewCell 1
+#define COCOAPODS_VERSION_PATCH_MCSwipeTableViewCell 2
+
+// PocketAPI
+#define COCOAPODS_POD_AVAILABLE_PocketAPI
+#define COCOAPODS_VERSION_MAJOR_PocketAPI 1
+#define COCOAPODS_VERSION_MINOR_PocketAPI 0
+#define COCOAPODS_VERSION_PATCH_PocketAPI 2
+
+// Shimmer
+#define COCOAPODS_POD_AVAILABLE_Shimmer
+#define COCOAPODS_VERSION_MAJOR_Shimmer 1
+#define COCOAPODS_VERSION_MINOR_Shimmer 0
+#define COCOAPODS_VERSION_PATCH_Shimmer 1
+
+// TUSafariActivity
+#define COCOAPODS_POD_AVAILABLE_TUSafariActivity
+#define COCOAPODS_VERSION_MAJOR_TUSafariActivity 1
+#define COCOAPODS_VERSION_MINOR_TUSafariActivity 0
+#define COCOAPODS_VERSION_PATCH_TUSafariActivity 0
+
+// TWTSideMenuViewController
+#define COCOAPODS_POD_AVAILABLE_TWTSideMenuViewController
+#define COCOAPODS_VERSION_MAJOR_TWTSideMenuViewController 0
+#define COCOAPODS_VERSION_MINOR_TWTSideMenuViewController 3
+#define COCOAPODS_VERSION_PATCH_TWTSideMenuViewController 0
+
+// libHN
+#define COCOAPODS_POD_AVAILABLE_libHN
+#define COCOAPODS_VERSION_MAJOR_libHN 3
+#define COCOAPODS_VERSION_MINOR_libHN 0
+#define COCOAPODS_VERSION_PATCH_libHN 0
+
